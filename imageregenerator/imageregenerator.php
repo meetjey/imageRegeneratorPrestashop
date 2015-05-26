6f4cce421638c632c0b3655c9bab5ba692f39988
meetjey <contact@meetjey.com>
2015-05-27 00:52:10 +0200

    Add watermark
    - watermark feature
    - code optimisation

--- imageregenerator/imageregenerator.php
+++ imageregenerator/imageregenerator.php
@@ -9,7 +9,7 @@ class ImageRegenerator extends Module
 		$this->bootstrap = true;
 		$this->name = 'imageregenerator';
 		$this->tab = 'administration';
-		$this->version = '1.0';
+		$this->version = '1.1';
 		$this->author = 'Jérémy Besson';
 		$this->need_instance = 0;
 		$this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.6');
@@ -22,7 +22,7 @@ class ImageRegenerator extends Module
 
 		$this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
 
-		if (!Configuration::get('IMAGEREGENERATOR'))      
+		if (!Configuration::get('IMAGEREGENERATOR'))
 			$this->warning = $this->l('No name provided');
 	}
 
@@ -107,15 +107,140 @@ class ImageRegenerator extends Module
 			}
 		}
 		$textHIW = $this->l("You can regenerate all your images safely.");
-		$r.='<div class="panel"><h3>'.$this->l("Let's go").'</h3><p>'.$textHIW.'</p><div class="clearfix"></div><table width="100%" id="autoImg-buttons"></table><div class="clearfix"></div>
-		<div class="btn-toolbar" role="toolbar"><div class="btn-group"><button class="btn btn-primary" id="image_regenerator-pause"><span class="icon-pause"></span> '.$this->l('PAUSE').'</button><button class="btn btn-success" id="image_regenerator-resume"><span class="icon-play"></span> '.$this->l('RESUME').'</button></div>
-		<div class="btn-group"><form method="post" id="image_regenerator_save_form"><input type="hidden" name="image_regenerator_queue_what" value=""/><input type="hidden" name="image_regenerator_queue" value=""/></form></div>
-		<div class="btn-group"><form method="post"><input type="hidden" name="image_regenerator_reinit" value="1"/><button type="submit" class="btn btn-warning" id="image_regenerator-reinit">'.$this->l('RESET').'</button></form></div></div>
-		</div><div class="panel"><h3>'.$this->l('Debug').'</h3><div id="autoImg-progress" style="width:100%;line-height:20px;height:60px;overflow:auto;"></div><br/><div class="clearfix"></div>
+		$r.='
+		<div class="panel">
+			<h3>'.$this->l("Let's go").'</h3>
+			<p>'.$textHIW.'</p>
+			<div class="clearfix"></div>
+			<table width="100%" id="autoImg-buttons"></table>
+			<div class="clearfix"></div>
+			<div class="btn-toolbar" role="toolbar">
+				<div class="btn-group">
+					<button class="btn btn-primary" id="image_regenerator-pause"><span class="icon-pause"></span> '.$this->l('PAUSE').'</button>
+					<button class="btn btn-success" id="image_regenerator-resume"><span class="icon-play"></span> '.$this->l('RESUME').'</button>
+				</div>
+				<div class="btn-group">
+					<form method="post" id="image_regenerator_save_form">
+						<input type="hidden" name="image_regenerator_queue_what" value=""/>
+						<input type="hidden" name="image_regenerator_queue" value=""/>
+					</form>
+				</div>
+				<div class="btn-group">
+					<form method="post">
+						<input type="hidden" name="image_regenerator_reinit" value="1"/>
+						<button type="submit" class="btn btn-warning" id="image_regenerator-reinit">'.$this->l('RESET').'</button>
+					</form>
+				</div>
+				<div class="btn-group">
+					<div class="checkbox">
+						<label><input type="checkbox" value="1" id="image_regenerator-watermark"> '.$this->l('Watermark ? (module watermark need to be enable)').'</label>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="panel"><h3>'.$this->l('Debug').'</h3><div id="autoImg-progress" style="width:100%;line-height:20px;height:60px;overflow:auto;"></div><br/><div class="clearfix"></div>
 		<script>var image_regenerator_can_run_queue = true;var image_regenerator_queuing_what = '.$image_regenerator_queue_what.';
 		var autoImg = $.parseJSON(\''.json_encode($list).'\');
-		var autoImgPath = "'._PS_BASE_URL_.__PS_BASE_URI__.'";
+		var autoImgPath = "'.$this->context->link->getAdminLink('AdminModules').'&configure='.$this->name.'";
 		</script></div></div>';
 		return $r;
+	}
+
+	function ajaxProcessRegenerateMethod(){
+		$process =array('categories' => _PS_CAT_IMG_DIR_,
+			'manufacturers' => _PS_MANU_IMG_DIR_,
+			'suppliers' => _PS_SUPP_IMG_DIR_,
+			'scenes' => _PS_SCENE_IMG_DIR_,
+			'products' => _PS_PROD_IMG_DIR_,
+			'stores' => _PS_STORE_IMG_DIR_
+			);
+		$baseType = Tools::getValue('type');
+		$type = ImageType::getImagesTypes($baseType);
+		$image = Tools::getValue('image');
+		$watermark = (int) Tools::getValue('watermark');
+		$dir = $process[$baseType];
+		$success = null;
+		$errors = null;
+		$watermarked = 0;
+		if($baseType!="products"){
+			if (preg_match('/^[0-9]*\.jpg$/', $image)){
+				foreach ($type as $k => $imageType)
+				{
+					// Customizable writing dir
+					$newDir = $dir;
+					if ($imageType['name'] == 'thumb_scene')
+						$newDir .= 'thumbs/';
+					if (!file_exists($newDir))
+						$errors = 1;
+					$newFile = $newDir.substr($image, 0, -4).'-'.stripslashes($imageType['name']).'.jpg';
+					if(file_exists($newFile) && !unlink($newFile))
+						$errors = 1;
+					if (!file_exists($newFile))
+					{
+
+						if (!file_exists($dir.$image) || !filesize($dir.$image))
+						{
+							$errors = sprintf(Tools::displayError('Source file does not exist or is empty (%s)', $dir.$image));
+						}
+						elseif (!ImageManager::resize($dir.$image, $newFile, (int)$imageType['width'], (int)$imageType['height']))
+						{
+							$errors = 1;
+						}else{
+							$success = 1;
+						}
+					}else{
+						$errors = 1;
+					}
+				}
+			}else{
+				$success=1;
+			}
+		}else{
+			$imageObj = new Image($image);
+			$existing_img = $dir.$imageObj->getExistingImgPath().'.jpg';
+			if (file_exists($existing_img) && filesize($existing_img))
+			{
+				if($watermark == 1){
+					$watermarked = 1;
+					$watermark = ModuleCore::getInstanceByName("watermark");
+					$valid_types = array();
+					$watermark_types = explode(',', Configuration::get('WATERMARK_TYPES'));
+					foreach($type as $k => $t){
+						if(in_array($t['id_image_type'],$watermark_types)){
+							$valid_types[] = $t;
+							unset($type[$k]);
+						}
+					}
+					if($watermark->hookActionWatermark(array('image_type'=>$valid_types,'id_image'=>$imageObj->id_image,'id_product'=>$imageObj->id_product))){
+						$success = 1;
+					}else{
+						$errors = 1;
+					}
+				}
+				if(count($type)>0){
+					foreach ($type as $imageType){
+						$newFile = $dir.$imageObj->getExistingImgPath().'-'.stripslashes($imageType['name']).'.jpg';
+						if(file_exists($newFile) && !unlink($newFile))
+							$errors = 1;
+						if (!file_exists($newFile)){
+							if (!ImageManager::resize($existing_img, $newFile, (int)($imageType['width']), (int)($imageType['height'])))
+							{
+								$errors = sprintf('Original image is corrupt (%s) or bad permission on folder', $existing_img);
+							}else{
+								$success = 1;
+							}
+						}else{
+							$errors = 1;
+						}
+					}
+				}
+			}
+			else
+			{
+				$errors = sprintf('Original image is missing or empty (%s)', $existing_img);
+			}
+		}
+		echo json_encode(array('success'=>$success,'error'=>$errors,'watermark'=>$watermarked));
+		exit;
 	}
 }
