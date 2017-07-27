e2a87a27e1e6db435b0fa0b24002fe8e92915734
ComonSoft <jean@comonsoft.com>
2017-07-27 18:18:30 +0200

    Create index.php
    Add default redirection in Prestashop dir

---
+++ imageregenerator/translations/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* History:
+*
+* 1.0 - First version
+*
+*  @author    Vincent MASSON <contact@coeos.pro>
+*  @copyright Vincent MASSON <www.coeos.pro>
+*  @license   http://www.coeos.pro/boutique/fr/content/3-conditions-generales-de-ventes
+*/
+
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
